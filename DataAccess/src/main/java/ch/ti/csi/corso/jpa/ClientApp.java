5703cded0574a5cbf5652ca9242e5d64e0dedac2
Docente06 <Docente06@PC007209M80.corsi.ac.ti.ch>
2017-06-21 10:37:42 +0200

    piccole correzioni (jpa)

--- DataAccess/src/main/java/ch/ti/csi/corso/jpa/ClientApp.java
+++ DataAccess/src/main/java/ch/ti/csi/corso/jpa/ClientApp.java
@@ -5,24 +5,29 @@ import java.util.Date;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 
 public class ClientApp {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("corsi");
 		EntityManager em = emf.createEntityManager();
-		
+
 		Studente s = new Studente();
 		s.setNome("Mario");
 		s.setCognome("Rossi");
 		s.setDataNascita(new Date());
-		s.setMatricola((int)Math.random() * 1000);
-		
-		em.getTransaction().begin();
-		em.persist(s);
-		em.getTransaction().commit();
+		s.setMatricola((int) Math.random() * 1000);
+		try {
+			em.getTransaction().begin();
+			em.persist(s);
+			em.getTransaction().commit();
+		} catch (PersistenceException ex) {
 
-		em.close();
+		} finally {
+			em.close();
+			emf.close();
+		}
 	}
 
 }
