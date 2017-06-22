9ab6fb50b76b7c69b75278aaf844ead53e25bcef
Docente06 <Docente06@PC007209M80.corsi.ac.ti.ch>
2017-06-22 11:24:05 +0200

    piccolo refactoring progetti batch

--- sbia/ch02/src/main/java/com/manning/sbia/ch02/GeneratesJobMetaData.java
+++ sbia/ch02/src/main/java/com/manning/sbia/ch02/GeneratesJobMetaData.java
@@ -17,7 +17,7 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * Generates some execution.
@@ -127,7 +127,7 @@ public class GeneratesJobMetaData {
 	}
 	
 	private static void truncate(ApplicationContext ctx) {
-		SimpleJdbcTemplate tpl = new SimpleJdbcTemplate(ctx.getBean(DataSource.class));
+		JdbcTemplate tpl = new JdbcTemplate(ctx.getBean(DataSource.class));
 		tpl.update("delete from BATCH_STEP_EXECUTION_CONTEXT");
 		tpl.update("delete from BATCH_JOB_EXECUTION_CONTEXT");
 		tpl.update("delete from BATCH_STEP_EXECUTION");
